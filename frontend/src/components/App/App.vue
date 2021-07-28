a4a54e46a7ba733c2ce11d7f67915d72ba2c83ee
mli42 <mli@student.42.fr>
2021-07-28 15:19:32 +0200

    Front: Install SASS (+) Clean default pages

---
+++ frontend/src/components/App/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  components: {
+  },
+}
+</script>
+
+<style lang="scss" src="./App.scss">
+</style>
