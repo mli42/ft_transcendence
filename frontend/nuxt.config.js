168d52eb0703053705c20233e0b2f3ea40bacca1
mli42 <mli@student.42.fr>
2021-08-01 22:25:10 +0200

    Front: fade transition

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -25,11 +25,15 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/Palette.scss',
+    '~/assets/transitions.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
+
+  pageTransition: 'fade',
+  layoutTransition: 'fade',
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
