f7cb1a6ee3992a75260b598d55b0c1cc210e8c99
mli42 <mli@student.42.fr>
2021-08-03 11:55:23 +0200

    Front: Login buttons ready

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -9,10 +9,14 @@
         <form>
           <LoginInput name="Username/Email" v-model="logEmail"></LoginInput>
           <LoginInput name="Password" v-model="logPass" :isPassword="true"></LoginInput>
-          <v-btn @click.prevent="loginSubmit"
-          block elevation="2">Login</v-btn>
+          <v-btn block elevation="2" class="logOpt" @click.prevent="loginSubmit">
+            <p class="v-btn-content">Login</p>
+          </v-btn>
         </form>
         <hr class="leftHR" />
+        <v-btn block elevation="2" class="logOpt" @click.prevent="login42">
+          <p class="v-btn-content">Connect with 42</p>
+        </v-btn>
       </div>
       <div class="contentCol">
         <h1 class="contentTitle">Sign-up</h1>
@@ -36,6 +40,9 @@ export default Vue.extend({
   methods: {
     loginSubmit(): void {
       console.log(this.logEmail, this.logPass);
+    },
+    login42(): void {
+      console.log("Logging with 42");
     },
   },
 });
