c848e5752dbd775f9d5a5677ba8e53769e62e5ce
Naila MBABAZI <nmbabazi@e1r11p22.42.fr>
2021-08-05 18:50:36 +0200

    css auth and password done

---
+++ frontend/pages/authentification.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="auth flexHVcenter">
+    <img alt="square logo" src="~/assets/img/logo500.png" />
+    <hr class="middleHR"/>
+    <div class="content">
+      <h1>Two-factor Authentification</h1>
+      <label for="code">Code :</label>
+      <input type="text" name="code" @input="updateValue($event.target.value)" />
+      <v-btn class="verify">
+        <p class="v-btn-content">Verify</p>
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'auth',
+  layout: 'empty',
+  data(): any {
+    return {
+      secretCode: "" as string,
+    };
+  },
+  methods: {
+    updateValue: function (value: string) {
+      // this.$emit('input', value);
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss" src="./authentification.scss">
+</style>
---
+++ frontend/pages/definePassword.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="auth flexHVcenter">
+    <img alt="square logo" src="~/assets/img/logo500.png" />
+    <hr class="middleHR"/>
+    <div class="content">
+      <h1>Define a new password</h1>
+      <label for="newPassword">Your new password :</label>
+      <input type="text" name="newPassword" @input="updateValue($event.target.value)" />
+      <v-btn class="verify">
+        <p class="v-btn-content">Verify</p>
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'newPassword',
+  layout: 'empty',
+  data(): any {
+    return {
+      newPassword: "" as string,
+    };
+  },
+  methods: {
+    updateValue: function (value: string) {
+      // this.$emit('input', value);
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss" src="./definePassword.scss">
+</style>
