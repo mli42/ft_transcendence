f669bc71ec63b9f1ca6b29bed15e78a42deb8778
Naila MBABAZI <nmbabazi@e1r11p19.42.fr>
2021-08-06 20:03:59 +0200

    modall start

---
+++ frontend/components/ModaleSettings/modalePhoto.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'modalPhoto',
+  data(): any {
+    return {
+
+    };
+  },
+  methods: {
+  }
+});
+</script>
+
+<style scoped lang="scss" src="./modaleSettings.scss">
+</style>
