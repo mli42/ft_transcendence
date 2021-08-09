54997a994aa3bba3784d334e272f99b8f1d0e3e8
mli42 <mli@student.42.fr>
2021-08-09 18:44:44 +0200

    Begin profile page

---
+++ frontend/components/Profile/Profile.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="profileMain">
+    <div>
+      <!-- Left side content (pp, buttons, friend list) -->
+      <img src="" :alt="`${this.username}'s profile picture`" class="pp" />
+    </div>
+
+    <div>
+      <p class="bigUsername">{{this.username}}</p>
+      <div class="primaryContent">
+        <div>
+          <Iconify iconName="ant-design:trophy-outlined"
+          param="color=white" class="BigIcon" ></Iconify>
+        </div>
+        <div>
+          <Iconify iconName="ant-design:clock-circle-outlined"
+          param="color=white" class="BigIcon" ></Iconify>
+        </div>
+      </div>
+      <div class="primaryContent"></div>
+      <div class="primaryContent"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Profile',
+  data() {
+    return {
+    };
+  },
+  props: ['username'],
+});
+</script>
+
+<style scoped lang="scss" src="./Profile.scss">
+</style>
