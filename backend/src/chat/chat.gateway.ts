8a3ed8f9ddc80f3cb292833dc778c65ae8048554
dmontgenie <doriane.montgenie@gmail.com>
2021-08-12 14:50:56 +0200

    chat connection

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -2,18 +2,9 @@ import { Logger } from '@nestjs/common';
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway()
-// export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-export class ChatGateway {
-
-    @SubscribeMessage('msgToServer')
-    handleMessage(client: Socket, text: string): WsResponse<string> {
-        this.logger.log('New message from a socket !');
-        return {event: 'msgToClient', data: text};
-        // this.server.emit('msgToClient', text);
-        // return { event: 'msgToClient', data: text };
-    }
-
+@WebSocketGateway({ cors: true })
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    
     @WebSocketServer() server: Server;
     private logger: Logger = new Logger('ChatGateway');
 
@@ -21,14 +12,18 @@ export class ChatGateway {
         this.logger.log('Initialized !')
     }
 
-    // handleDisconnect(client: Socket) {
-    //     this.logger.log(`Client diconnect: ${client.id}`);
-    // }
+    @SubscribeMessage('msgToServer')
+    handleMessage(client: Socket, text: string): WsResponse<string> {
+        this.logger.log('New message from a socket !');
+        return {event: 'msgToClient', data: text};
+    }
+
+    handleDisconnect(client: Socket) {
+        this.logger.log(`Client diconnect: ${client.id}`);
+    }
 
     handleConnection(client: Socket, ...args: any[]) {
         this.logger.log(`Client connected: ${client.id}`);
     }
-
-    
 
 }
