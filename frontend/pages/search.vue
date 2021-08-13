39f2ed874a38f0f251106f3d8bdd9e8a3358e0ae
mli42 <mli@student.42.fr>
2021-08-13 11:47:12 +0200

    Front search: use query

--- frontend/pages/search.vue
+++ frontend/pages/search.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-      <backgroundAnim></backgroundAnim>
+    <backgroundAnim></backgroundAnim>
+    <div class="searchPage useWholePage">
+      <div class="flexAlignCol">
+        <p class="title">Search</p>
+        <input class="search" type="text" name="whoInput" id="whoInput" v-model="who">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,14 +15,31 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'search',
-  layout: 'default',
   data() {
     return {
+      who: '' as string,
+      users: undefined as any,
+    };
+  },
+  mounted() {
+    const who: any = this.$route.query.who;
+    if (typeof who == 'string') {
+      this.who = who;
+      this.fetchData();
     }
-  }
+  },
+  methods: {
+    async fetchData() {
+      this.users = await this.$axios
+        .get(`/api/user/search?username=${this.who}`)
+        .then((res: any) => res.data)
+        .catch(() => []);
+      this.$router.push(`/search?who=${this.who}`);
+      // console.log('Itsa me Mario:', this.users, this.who);
+    },
+  },
 });
 </script>
 
-<style scoped>
-
+<style scoped lang="scss" src="./search.scss">
 </style>
