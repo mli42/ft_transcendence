2552d84fdeb7e30ddfa6b7973949a032cc71481e
mli42 <mli@student.42.fr>
2021-08-24 22:20:37 +0200

    Front: clean middleware/auth + Use enum + Redirect if go to auth route w/o need

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -1,25 +1,32 @@
+enum elogState {
+  logged,
+  need2FA,
+  loggedout,
+};
+
+function isLoginCatch(err: any): elogState {
+  if (err.response.data.message === 'need 2FA')
+    return elogState.need2FA;
+  return elogState.loggedout;
+}
+
 export default async function (context: any) {
   const isLoginPage = (context.route.fullPath === '/login');
   const is2faPage: boolean = (context.route.fullPath === '/authentification');
+  const isLogged: elogState = await context.app.$axios.get('/api/user/isLogin')
+  .then(() => elogState.logged)
+  .catch(isLoginCatch);
 
-  const isLogged: string = await context.app.$axios.get('/api/user/isLogin')
-  .then(() => 'OK')
-  .catch((err: any) => {
-    if (err.response.data.message === 'need 2FA') {
-      return '2FA';
-    }
-    return 'NO';
-  });
-  context.store.commit('updateLogState', (isLogged === 'OK'));
+  context.store.commit('updateLogState', (isLogged === elogState.logged));
 
-  if (isLogged == '2FA') {
+  if (isLogged === elogState.need2FA) {
     if (is2faPage === false)
       context.redirect('/authentification');
     return;
   }
 
-  if (isLogged == 'OK') {
-    if (isLoginPage)
+  if (isLogged === elogState.logged) {
+    if (isLoginPage || is2faPage)
       context.redirect('/');
   } else { // Not logged
     if (!isLoginPage)
