8f13e2b0e577d0141990dc7b33344ad6e845841a
jean-xavierr <religieuxjxavier@gmail.com>
2021-08-25 17:00:56 +0200

    :art: : Refractor MatchHistory -> GameHistory

--- backend/src/game/entities/matchHistory.entity.ts
+++ backend/src/game/entities/matchHistory.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
-export class MatchHistory {
+export class GameHistory {
 
     @PrimaryGeneratedColumn("uuid")
     gameId: string;
