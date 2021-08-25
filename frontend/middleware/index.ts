1fed17025ccb65dcba00d85113f90a2ec4889fb8
mli42 <mli@student.42.fr>
2021-08-25 18:23:17 +0200

    Admin route

--- frontend/middleware/index.ts
+++ frontend/middleware/index.ts
@@ -1,3 +1,12 @@
+function adminGuard(context: any, user: any): void {
+  const isAdminView: boolean = (context.route.fullPath === '/admin');
+
+  if (isAdminView && user.isAdmin === false) {
+    context.redirect('/');
+    context.$mytoast.err('Unauthorized');
+  }
+}
+
 export default async function (context: any) {
   if (context.store.state.isLogged == false)
     return ;
@@ -6,6 +15,7 @@ export default async function (context: any) {
   const avatarBaseURL: string = context.$user.avatarBaseURL;
   const avatarURL: string = `${avatarBaseURL}/${user.profile_picture}`;
 
+  adminGuard(context, user);
   context.store.commit('updateUser', user);
   context.store.commit('updateAvatarURL', avatarURL);
 };
---
+++ frontend/pages/admin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'admin',
+  head(): object {
+    return {
+      title: "Admin View" as String,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss" src="./admin.scss">
+</style>
