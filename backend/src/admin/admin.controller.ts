76be419f1bfb431cebf5e0b7737ea3e1a5273ccb
jean-xavierr <religieuxjxavier@gmail.com>
2021-08-26 18:32:41 +0200

    :arrow_up: : Add guard 2FA for Admin view and update method updateIsAdmin

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Req, UnauthorizedException, UseGuards } from '@nestjs/
 import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/user/entities/user.entity';
+import { UserAuth } from 'src/user/guards/userAuth.guard';
 import { AdminService } from './admin.service';
 
 @ApiTags('users')
@@ -12,7 +13,7 @@ export class AdminController {
     ) {}
 
     @ApiOperation({description: 'Get numbers Users'})
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(AuthGuard('jwt'), UserAuth)
     @Get('/allUsers')
     getNumbersUsers(@Req() req): Promise<number> {
         const user: User = req.user;
@@ -23,7 +24,7 @@ export class AdminController {
     }
 
     @ApiOperation({description: 'Get All Admin'})
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(AuthGuard('jwt'), UserAuth)
     @Get('/allAdmin')
     getAllAdmin(@Req() req): Promise<Partial<User[]>> {
         const user: User = req.user;
