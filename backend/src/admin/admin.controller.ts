4f21f44c97a9c01bbeb314446bda243a25f7677d
jean-xavierr <religieuxjxavier@gmail.com>
2021-08-26 13:49:23 +0200

    :arrow_up: : Update Admin controller | Check if user isAdmin

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/user/entities/user.entity';
 import { AdminService } from './admin.service';
 
@@ -10,13 +11,25 @@ export class AdminController {
         private readonly adminService: AdminService
     ) {}
 
+    @ApiOperation({description: 'Get numbers Users'})
+    @UseGuards(AuthGuard('jwt'))
     @Get('/allUsers')
-    getNumbersUsers(): Promise<number> {
+    getNumbersUsers(@Req() req): Promise<number> {
+        const user: User = req.user;
+
+        if (user.isAdmin === false)
+			throw new UnauthorizedException('You aren\'t an administrator');
         return this.adminService.getNumbersUsers();
     }
 
+    @ApiOperation({description: 'Get All Admin'})
+    @UseGuards(AuthGuard('jwt'))
     @Get('/allAdmin')
-    getAllAdmin(): Promise<Partial<User[]>> {
+    getAllAdmin(@Req() req): Promise<Partial<User[]>> {
+        const user: User = req.user;
+
+        if (user.isAdmin === false)
+			throw new UnauthorizedException('You aren\'t an administrator');
         return this.adminService.getAllAdmin();
     }
 }
