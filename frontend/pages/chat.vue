73080c5e6f7acffbe9503a8b2632895cbcaf7109
ejawe <doriane.montgenie@gmail.com>
2021-09-01 18:32:00 +0200

    Chat: error password channel

--- frontend/pages/chat.vue
+++ frontend/pages/chat.vue
@@ -146,18 +146,24 @@ export default Vue.extend({
       }
     },
     sendPassword() {
-      this.$user.socket.emit('passwordChannel', this.channels[this.selectedChannel], this.password, (data: any) => {
-        if (data === false)
-        {
-          this.modalBool.showPrivacy = false;
-          console.log("wrong mdp!!");
-        }
-        else
-        {
-          this.$user.socket.emit('joinChannel', this.channels[this.selectedChannel]);
-          this.currentChannel = this.channels[this.selectedChannel];
-        }
-      });
+      this.$user.socket.emit('passwordChannel', this.channels[this.selectedChannel], this.password)
+      console.log("avant");
+      // (data: any) => {
+      //   console.log("avant");
+      //   if (data === false)
+      //   {
+      //     this.modalBool.showPrivacy = false;
+      //     console.log("wrong mdp!!");
+      //   }
+      //   else
+      //   {
+      //     console.log("else!!");
+      //     this.$user.socket.emit('joinChannel', this.channels[this.selectedChannel]);
+      //     this.currentChannel = this.channels[this.selectedChannel];
+      //   }
+      //   console.log("la!!");
+      // });
+      console.log("la!!");
       this.password = '';
     },
     sendMsg(): void {
