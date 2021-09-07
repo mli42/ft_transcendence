cf1e4db033bbfd098e35954cf2645936cc1847d0
mli42 <mli@student.42.fr>
2021-09-07 18:22:22 +0200

    Game route use UUID as gameID

--- frontend/pages/game/index.vue
+++ frontend/pages/game/index.vue
@@ -9,22 +9,15 @@ import Vue from 'vue';
 export default Vue.extend ({
   name: 'indexGamePage',
   mounted() {
-    window.$nuxt.$router.push('/game/' + this.makeId(10)); // Redirect client
-  },
-  methods: {
-    makeId(length: number): string {
-      let result: string = '';
-      let characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      let charactersLength: number = characters.length;
-      for ( let i: number = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return result;
-    }
+    this.$axios.get('/api/game/uuid')
+    .then((res: any) => {
+      const uuid: string = res.data;
+      this.$router.push(`/game/${uuid}`);
+    })
+    .catch(this.$mytoast.defaultCatch);
   },
 });
 </script>
 
 <style scoped lang="scss">
-
 </style>
