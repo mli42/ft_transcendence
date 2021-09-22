d8e2964e58bde0d61da29e6bab75ad603f259c3f
mli42 <mli@student.42.fr>
2021-09-22 22:29:35 +0200

    Link on playing icon

--- frontend/components/Profile/FriendCard/FriendCard.vue
+++ frontend/components/Profile/FriendCard/FriendCard.vue
@@ -7,8 +7,10 @@
       <NuxtLink :to="userProfile">
         <p>{{shortUsername}}</p>
       </NuxtLink>
-      <Iconify v-if="isPlaying" title="Currently playing"
-      iconName="ri:sword-line" class="playing"></Iconify>
+      <NuxtLink v-if="isPlaying" :to="gameURL">
+        <Iconify title="Currently playing"
+        iconName="ri:sword-line" class="playing"></Iconify>
+      </NuxtLink>
     </div>
     <p v-else-if="$fetchState.error">Oh no, an error occured...</p>
   </div>
@@ -37,9 +39,17 @@ export default Vue.extend({
     shortUsername(): string {
       return (this.$user.shortName(this.user.username));
     },
+    playingUsers(): any {
+      return this.$store.state.playingUsers;
+    },
     isPlaying(): boolean {
-      const keys: any = Object.keys(this.$store.state.playingUsers);
+      const keys: any = Object.keys(this.playingUsers);
       return keys.includes(this.userId);
+    },
+    gameURL(): string {
+      if (this.isPlaying == false)
+        return '';
+      return `/game/${this.playingUsers[this.userId]}`;
     },
   },
   methods: {
